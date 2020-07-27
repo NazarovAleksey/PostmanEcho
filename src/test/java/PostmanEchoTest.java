aded7610b32d6b5ce8776fcd4289e1df42e0a9be
Alexey Nazarov <alexeyka1987@gmail.com>
2020-07-27 21:19:41 +0500

    fault

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
+    @Test
+    void shouldReturnPostmanEcho() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("fault"));
+    }
 }
