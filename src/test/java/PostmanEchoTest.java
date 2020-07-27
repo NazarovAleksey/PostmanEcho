ea267c6025ad548be54d2ff2e233b35802418fb8
Alexey Nazarov <alexeyka1987@gmail.com>
2020-07-27 21:22:31 +0500

    success

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,6 +15,6 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("fault"));
+                .body("data", equalTo("some value"));
     }
 }
